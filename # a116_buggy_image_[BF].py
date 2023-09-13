0a5391bfc71c656efd8895751d0a9905dc017bae
TheBenicyhot <142819378+TheBenicyhot@users.noreply.github.com>
2023-09-13 09:39:17 -0600

    Add files via upload

--- # a116_buggy_image_[BF].py
+++ # a116_buggy_image_[BF].py
@@ -2,19 +2,21 @@
 import turtle as trtl
 # instead of a descriptive name of the turtle such as painter,
 # a less useful variable name x is used
-x = trtl.Turtle()
-x.pensize(40)
-x.circle(20)
-w = 6
-y = 70
-z = 380 / w
-x.pensize(5)
-n = 0
-while (n < w):
-  x.goto(0,0)
-  x.setheading(z*n)
-  x.forward(y)
-  n = n + 1
-x.hideturtle()
+spider = trtl.Turtle()
+spider.pensize(40)
+spider.circle(20)
+
+spider_legs = 6
+spider_leg_length = 70
+leg_distance  = 380 / spider_legs
+spider.pensize(5)
+counter = 0
+while (counter < spider_legs):
+  spider.goto(0,0)
+  spider.setheading(leg_distance *counter)
+  spider.forward(spider_leg_length)
+  counter = counter + 1
+spider.hideturtle()
+
 wn = trtl.Screen()
 wn.mainloop()
