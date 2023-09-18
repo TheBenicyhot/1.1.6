e66ac641eeb2eea5301e335efd52db3c4e10699e
TheBenicyhot <142819378+TheBenicyhot@users.noreply.github.com>
2023-09-18 09:40:24 -0600

    Update # a116_buggy_image_[BF].py

--- # a116_buggy_image_[BF].py
+++ # a116_buggy_image_[BF].py
@@ -6,21 +6,35 @@ import turtle as trtl
 
 # this renames the turtle making it spider
 spider = trtl.Turtle()
-# this makes the spider draw at a huge size which is then turned to a circle
+# Create a spider body
 spider.pensize(40)
 spider.circle(20)
-# this defines the rest of the important varibales
-spider_legs = 6
+# Configure spider legs
+spider_legs = 4
 spider_leg_length = 70
-leg_distance  = 380 / spider_legs
+leg_distance  = 100 / spider_legs
 spider.pensize(5)
 counter = 0
-
+#Draw legs
 while (counter < spider_legs):
-  spider.goto(0,0)
+  spider.goto(0,20)
   spider.setheading(leg_distance *counter)
   spider.forward(spider_leg_length)
   counter = counter + 1
+
+while(counter < 8):
+  spider.goto(0,20)
+  spider.setheading(leg_distance *counter - 260)
+  spider.forward(spider_leg_length)
+  counter = counter + 1
+spider.penup()
+spider.goto(20,20)
+spider.pendown()
+spider.pencolor("red")
+spider.pensize(3)
+spider.circle(8)
+spider.
+#hides the tutlre after the drawing is done 
 spider.hideturtle()
 
 wn = trtl.Screen()
